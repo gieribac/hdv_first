cc545d8b42e19d04207444916e76318f7806d48d
gieribac <78530133+gieribac@users.noreply.github.com>
2022-03-10 01:08:43 -0500

    coreccion responsive sm y cv 1

--- cv/js_/main.js
+++ cv/js_/main.js
@@ -2,9 +2,9 @@
 const btnToggle = document.querySelector('.toggle-btn');
 
 btnToggle.addEventListener('click', function () {
-  console.log('clik')
+
   document.getElementById('sidebar').classList.toggle('active');
   document.querySelector('.toggle-btn').classList.toggle('active');
   document.querySelector('.contenido').classList.toggle('active');
-  console.log(document.getElementById('sidebar'))
+
 });
